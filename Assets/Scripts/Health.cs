615a62a240f451700860053434c56bf3cc4dad59
AlexeyTorpeda <103385895+AlexeyTorpeda@users.noreply.github.com>
2023-06-01 15:41:08 +0300

    changes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,13 +1,10 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    private float _maxHealth = 100f;
-    private float _minHealth = 0f;
-    private float _healthChangeDelay = 50f;
+    [SerializeField] private float _maxHealth = 100f;
+    [SerializeField] private float _minHealth = 0f;
 
     private float _currentHealth;
 
@@ -18,26 +15,20 @@ public class Health : MonoBehaviour
         _currentHealth = _maxHealth;
     }
 
-    public void Heal(float amount)
-    {
-        StartCoroutine(ChangeHealth(amount));
-    }
-
     public void Damage(float amount)
     {
-        StartCoroutine(ChangeHealth(-amount));
+        ChangeHealth(-amount);
     }
 
-    private IEnumerator ChangeHealth(float amount)
+    public void Heal(float amount)
+    {
+        ChangeHealth(amount);
+    }
+
+    private void ChangeHealth(float amount)
     {
         float targetHealth = Mathf.Clamp(_currentHealth + amount, _minHealth, _maxHealth);
-
-        while (_currentHealth != targetHealth)
-        {
-            float newHealth = Mathf.MoveTowards(_currentHealth, targetHealth, Time.deltaTime * _healthChangeDelay);
-            HealthChanged?.Invoke(newHealth, _maxHealth);
-            _currentHealth = newHealth;
-            yield return null;
-        }
+        _currentHealth = targetHealth;
+        HealthChanged?.Invoke(_currentHealth, _maxHealth);
     }
 }
